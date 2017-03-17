6910c447c5097fbd3168c19eb0cedbcfb6f412c4
cristian <cristian.fonolla@gmail.com>
2017-03-17 17:24:13 +0100

    Initial version

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <weather></weather>
+  </div>
+</template>
+
+<script>
+import Weather from './components/Weather'
+
+export default {
+  name: 'app',
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+* {
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Roboto, Arial;
+  font-size: 1.2em;
+  color: #666;
+  overflow-y: hidden;
+}
+</style>
